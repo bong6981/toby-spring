7e9f4fe02c48e53b17a42f3d029549b6d582b607
bong6981 <kbk5190@naver.com>
2022-03-04 22:46:37 +0900

    1.2.2 커넥션 만들기의 추출

--- vol1/practice1/tobyspring/src/main/java/springstudy/tobyspring/user/dao/UserDao.java
+++ vol1/practice1/tobyspring/src/main/java/springstudy/tobyspring/user/dao/UserDao.java
@@ -6,10 +6,7 @@ import springstudy.tobyspring.user.domain.User;
 
 public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
-        //Loading class `com.mysql.jdbc.Driver'. This is deprecated. The new driver class is `com.mysql.cj.jdbc.Driver'.
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/tobyspringvol1", "scott", "tiger");
-
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
@@ -22,8 +19,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/tobyspringvol1", "scott", "tiger");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
@@ -40,6 +36,12 @@ public class UserDao {
         c.close();
 
         return user;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        //Loading class `com.mysql.jdbc.Driver'. This is deprecated. The new driver class is `com.mysql.cj.jdbc.Driver'.
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost/tobyspringvol1", "scott", "tiger");
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
