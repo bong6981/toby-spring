7fdf9187ae3f34230e49983093308b3b013fb96c
bong6981 <kbk5190@naver.com>
2022-03-10 16:10:39 +0900

    1.4 제어의 역전(IoC) - 1.4.2 오브젝트 팩토리의 활용

--- vol1/practice1/tobyspring/src/main/java/springstudy/tobyspring/user/dao/DaoFactory.java
+++ vol1/practice1/tobyspring/src/main/java/springstudy/tobyspring/user/dao/DaoFactory.java
@@ -2,8 +2,10 @@ package springstudy.tobyspring.user.dao;
 
 public class DaoFactory {
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = new NConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-        return userDao;
+        return new UserDao(connectionMaker());
+    }
+
+    private ConnectionMaker connectionMaker() {
+        return new NConnectionMaker();
     }
 }
