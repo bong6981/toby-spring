a04aabca1907c6619b482026a1ee68af6d8d0727
bong6981 <kbk5190@naver.com>
2022-03-10 16:00:40 +0900

    1.4 제어의 역전(IoC) - 1.4.1 오브젝트 팩토리

--- vol1/practice1/tobyspring/src/main/java/springstudy/tobyspring/user/dao/UserDaoTest.java
+++ vol1/practice1/tobyspring/src/main/java/springstudy/tobyspring/user/dao/UserDaoTest.java
@@ -6,19 +6,18 @@ import springstudy.tobyspring.user.domain.User;
 
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ConnectionMaker connectionMaker = new NConnectionMaker();
-        UserDao dao = new UserDao(connectionMaker);
+        UserDao userDao = new DaoFactory().userDao();
 
         User user = new User();
         user.setId("whiteship");
         user.setName("백기선");
         user.setPassword("married");
 
-        dao.add(user);
+        userDao.add(user);
 
         System.out.println(user.getId() + " 등록 성공");
 
-        User user2 = dao.get(user.getId());
+        User user2 = userDao.get(user.getId());
         System.out.println("user2 = " + user2.getName());
         System.out.println("user2.getPassword() = " + user2.getPassword());
 
