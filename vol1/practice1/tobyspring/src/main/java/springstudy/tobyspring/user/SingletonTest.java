b2654e1e92d93c700dfb819a48b43cfa51842fe5
bong6981 <kbk5190@naver.com>
2022-03-13 00:08:47 +0900

    1.6 싱글톤 레지스트리- 1.6.1 싱글톤 레지스트리로서의 애플리케이션 컨택스트

---
+++ vol1/practice1/tobyspring/src/main/java/springstudy/tobyspring/user/SingletonTest.java
@@ -0,0 +1,26 @@
+package springstudy.tobyspring.user;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import springstudy.tobyspring.user.dao.DaoFactory;
+import springstudy.tobyspring.user.dao.UserDao;
+
+public class SingletonTest {
+    public static void main(String[] args) {
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao1 = daoFactory.userDao();
+        UserDao userDao2 = daoFactory.userDao();
+
+        System.out.println("userDao1 = " + userDao1);
+        System.out.println("userDao2 = " + userDao2);
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao userDao3 = context.getBean("userDao", UserDao.class);
+        UserDao userDao4 = context.getBean("userDao", UserDao.class);
+
+        System.out.println("userDao3 = " + userDao3);
+        System.out.println("userDao4 = " + userDao4);
+        System.out.println("(userDao4==userDao3) = " + (userDao4==userDao3));
+    }
+}
