0df701663edc4f8c6043e5b9fefbf29f2e10d3d0
bong6981 <kbk5190@naver.com>
2022-04-03 06:49:41 +0900

    1.7 의존관계주입(DI) - 1.7.4 의존관계 주입의 응용

---
+++ vol1/practice1/tobyspring/src/main/java/springstudy/tobyspring/user/dao/CountingConnectionMaker.java
@@ -0,0 +1,23 @@
+package springstudy.tobyspring.user.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class CountingConnectionMaker implements ConnectionMaker{
+    int counter = 0;
+    private ConnectionMaker realConnectionMaker;
+
+    public CountingConnectionMaker(ConnectionMaker realConnectionMaker) {
+        this.realConnectionMaker = realConnectionMaker;
+    }
+
+    @Override
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        this.counter++;
+        return realConnectionMaker.getConnection();
+    }
+
+    public int getCounter() {
+        return this.counter;
+    }
+}
