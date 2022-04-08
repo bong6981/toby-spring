7972f06846925e5913f1aa59f1c397f6e7febeae
bong6981 <kbk5190@naver.com>
2022-04-08 09:08:46 +0900

    2.2 userdaotest개선

--- vol1/practice1/tobyspring/src/main/java/springstudy/tobyspring/user/dao/UserDaoTest.java
+++ vol1/practice1/tobyspring/src/main/java/springstudy/tobyspring/user/dao/UserDaoTest.java
@@ -1,33 +1,36 @@
 package springstudy.tobyspring.user.dao;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.sql.SQLException;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import springstudy.tobyspring.user.domain.User;
 
 public class UserDaoTest {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
+
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
         UserDao userDao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
-        user.setId("whiteship5");
-        user.setName("백기선");
-        user.setPassword("married");
+        user.setId("gyumee");
+        user.setName("박성철");
+        user.setPassword("springno1");
 
         userDao.add(user);
 
-        System.out.println(user.getId() + " 등록 성공");
-
         User user2 = userDao.get(user.getId());
-        System.out.println("user2 = " + user2.getName());
-        System.out.println("user2.getPassword() = " + user2.getPassword());
+
+        assertThat(user2.getName()).isEqualTo(user.getName());
+        assertThat(user2.getPassword()).isEqualTo(user.getPassword());
 
         System.out.println(user2.getId() + " 조회 성공");
     }
