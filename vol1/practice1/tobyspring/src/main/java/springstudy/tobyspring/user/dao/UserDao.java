b62916f85dd80710ab028394b8db5d4e4b4a6dac
bong6981 <kbk5190@naver.com>
2022-05-06 09:28:59 +0900

    3.2 변하는 것과 변하지 않는 것

--- vol1/practice1/tobyspring/src/main/java/springstudy/tobyspring/user/dao/UserDao.java
+++ vol1/practice1/tobyspring/src/main/java/springstudy/tobyspring/user/dao/UserDao.java
@@ -112,6 +112,30 @@ public class UserDao {
         c.close();
     }
 
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = dataSource.getConnection();
+            ps = stmt.makePreparedStatement(c);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if(ps!=null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {}
+            }
+            if(c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {}
+            }
+        }
+    }
+
     public int getCount() throws SQLException {
         Connection c = null;
         PreparedStatement ps = null;
